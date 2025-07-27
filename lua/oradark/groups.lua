8af7514a4fc2331ccccc6d2728430719ae666ea5
devEducaua <schokito4@gmail.com>
2025-07-26 23:32:58 -0300

    style: fix indentation erros in palette.lua and organization on groups.lua

--- lua/oradark/groups.lua
+++ lua/oradark/groups.lua
@@ -5,16 +5,20 @@ local colors = require("oradark.palette")
 M.setup = function ()
     return {
         Normal = { fg = colors.light_g, bg = colors.bg },
+        Changed = { fg = colors.orange },
+        DiagnosticInfo = { fg = colors.orange },
+        LineNr = { fg = colors.dark },
 
         Comment = { fg = colors.dark, italic = true },
         String = { fg = colors.orange },
         Number = { fg = colors.light_g },
         Keyword = { fg = colors.grey, bold = true },
-        LineNr = { fg = colors.dark },
+        Function = { fg = colors.orange },
+
         CursorLine = { bg = colors.bg },
         StatusLine = { fg = colors.fg, bg = colors.bg },
+
         Visual = { bg = colors.bg, fg = colors.fg },
-        Function = { fg = colors.orange },
         Property = { fg = colors.dark },
         Method = { fg = colors.light_g },
         Attribute = { fg = colors.light_g },
@@ -23,7 +27,10 @@ M.setup = function ()
         Special = { fg = colors.punctuation },
         Identifier = { fg = colors.grey },
 
+        Question = { fg = colors.orange },
+
         ["@variable"] = { fg = colors.light_g },
+        ["@markup.heading.1.markdown"] = { fg = colors.orange },
 
         Oilfile = { fg = colors.light },
         OilfileHidden = { fg = colors.light },
--- lua/oradark/palette.lua
+++ lua/oradark/palette.lua
@@ -1,12 +1,12 @@
 return {
-        bg = "#000000",
-        fg = "#ffffff",
-        -- red = "#300404",
-        -- str = "#a43c35",
-        light = "#e5e5e5",
-        light_g = "#777777",
-        orange = "#d17e25",
-        grey = "#444444",
-        dark = "#333333",
-        punctuation = "#7a7a7a"
-    }
+    bg = "#000000",
+    fg = "#ffffff",
+    -- red = "#300404",
+    -- str = "#a43c35",
+    light = "#e5e5e5",
+    light_g = "#777777",
+    orange = "#d17e25",
+    grey = "#444444",
+    dark = "#333333",
+    punctuation = "#7a7a7a"
+}
