fb6c1776241bb31a1eff9a73a4a0d330b100b000
devEducaua <schokito4@gmail.com>
2025-07-28 04:54:45 -0300

    feat(groups): improve ui experience and treesitter support

--- lua/oradark/groups.lua
+++ lua/oradark/groups.lua
@@ -4,22 +4,26 @@ local colors = require("oradark.palette")
 
 M.setup = function ()
     return {
+        -- No Code
         Normal = { fg = colors.light_g, bg = colors.bg },
         Changed = { fg = colors.orange },
         DiagnosticInfo = { fg = colors.orange },
         LineNr = { fg = colors.dark },
         ModeMsg = { fg = colors.orange },
+        MoreMsg = { fg = colors.orange },
+        Directory = { fg = colors.grey },
+        Question = { fg = colors.orange },
+        Visual = { bg = colors.light_g, fg = colors.bg },
+        -- Cursor = { bg = colors.orange, fg = colors.bg },
+        Pmenu = { bg = colors.bg, fg = colors.light_g },
+        PmenuSel = { bg = colors.dark, fg = colors.bg },
 
+        -- Code
         Comment = { fg = colors.dark, italic = true },
         String = { fg = colors.orange },
         Number = { fg = colors.light_g },
         Keyword = { fg = colors.grey, bold = true },
         Function = { fg = colors.orange },
-
-        CursorLine = { bg = colors.bg },
-        StatusLine = { fg = colors.fg, bg = colors.bg },
-
-        Visual = { bg = colors.bg, fg = colors.fg },
         Property = { fg = colors.dark },
         Method = { fg = colors.light_g },
         Attribute = { fg = colors.light_g },
@@ -27,16 +31,25 @@ M.setup = function ()
         Delimiter = { fg = colors.light_g },
         Special = { fg = colors.punctuation },
         Identifier = { fg = colors.grey },
+        Type = { fg = colors.orange },
 
-        Question = { fg = colors.orange },
+        -- Line
+        CursorLine = { bg = colors.cursor_line },
+        StatusLine = { fg = colors.fg, bg = colors.bg },
 
+        -- Treesitter
         ["@variable"] = { fg = colors.light_g },
+        ["@type.builtin.typescript"] = { fg = colors.light },
         ["@markup.heading"] = { fg = colors.orange },
+        ["@markup.list.checked.markdown"] = { fg = colors.orange },
 
+        -- Telescope
+        TelescopeSelection = { fg =  colors.bg, bg = colors.grey },
+
+        -- Oil
         Oilfile = { fg = colors.light },
         OilfileHidden = { fg = colors.light },
         OilDirHidden = { fg = colors.grey },
-        Directory = { fg = colors.grey }
     }
 end
 
