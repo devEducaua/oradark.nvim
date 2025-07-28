a08cf201e945b8543a3585ec3f5162d7b3be9e35
devEducaua <schokito4@gmail.com>
2025-07-28 20:14:56 -0300

    feat(extra): add port for fish

---
+++ extra/oradark.fish
@@ -0,0 +1,10 @@
+set -l fg 777777
+set -l orange d17e25
+set -l red f52a65
+
+set -g fish_color_normal $fg
+set -g fish_color_command $orange
+set -g fish_color_param $fg
+set -g fish_color_keyword $fg
+set -g fish_color_quote $orange
+set -g fish_color_error $red
