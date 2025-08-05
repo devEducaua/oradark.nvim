c75e2b1151e59f7f934bc7845df02445dd62ca57
devEducaua <schokito4@gmail.com>
2025-08-05 20:00:08 -0300

    feat: add more basic colors

--- lua/oradark/palette.lua
+++ lua/oradark/palette.lua
@@ -1,11 +1,16 @@
 return {
     bg = "#000000",
     fg = "#ffffff",
-    cursor_line = "#111111",
-    orange = "#d17e25",
+
+    non_text = "#1e1e1e",
     light = "#e5e5e5",
     light_g = "#777777",
+    dark_g  = "#7a7a7a",
     grey = "#444444",
     dark = "#333333",
-    punctuation = "#7a7a7a"
+
+    orange = "#d17e25",
+    red = "#d14d4d",
+    yellow = "#e6c65c",
+    green = "#65a765"
 }
